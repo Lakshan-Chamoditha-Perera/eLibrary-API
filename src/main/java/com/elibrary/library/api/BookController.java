01675271f744d6293344dd2629c416d5e4074e73
Lakshan Chamoditha Perera <perera.alc2000@gmail.com>
2024-01-25 11:45:23 +0530

    Book CRUD - API layer

--- src/main/java/com/elibrary/library/api/BookController.java
+++ src/main/java/com/elibrary/library/api/BookController.java
@@ -1,13 +1,15 @@
 package com.elibrary.library.api;
 
+import com.elibrary.library.dto.AuthorDto;
+import com.elibrary.library.dto.BookDto;
 import com.elibrary.library.payload.StandardResponse;
 import com.elibrary.library.service.custom.BookService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/books")
@@ -46,5 +48,79 @@ public class BookController {
                             .message(e.getMessage()).build());
         }
     }
+    @PostMapping("/save")
+    public ResponseEntity<StandardResponse> save(@Valid @RequestBody BookDto dto, BindingResult bindingResult) {
+        try {
+            if (bindingResult.hasErrors()) {
+                return ResponseEntity.status(
+                        HttpStatus.BAD_REQUEST).body(
+                        new StandardResponse(500, "Fail", bindingResult.getAllErrors()));
+            }
+            return ResponseEntity.ok(
+                    StandardResponse.builder()
+                            .code(200)
+                            .message("Success")
+                            .data(bookService.save(dto)).build());
 
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(
+                    StandardResponse.builder()
+                            .code(500)
+                            .message(e.getMessage()).build());
+        }
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<StandardResponse> search(@RequestParam String isbn) {
+        try {
+            return ResponseEntity.ok(
+                    StandardResponse.builder()
+                            .code(200)
+                            .message("Success")
+                            .data(bookService.get(isbn)).build());
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(
+                    StandardResponse.builder()
+                            .code(500)
+                            .message(e.getMessage()).build());
+        }
+    }
+
+    @PatchMapping
+    public ResponseEntity<StandardResponse> update(@Valid @RequestBody BookDto dto, BindingResult bindingResult) {
+        try {
+            if (bindingResult.hasErrors()) {
+                return ResponseEntity.status(
+                        HttpStatus.BAD_REQUEST).body(
+                        new StandardResponse(500, "Fail", bindingResult.getAllErrors()));
+            }
+            return ResponseEntity.ok(
+                    StandardResponse.builder()
+                            .code(200)
+                            .message("Success")
+                            .data(bookService.update(dto)).build());
+
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(
+                    StandardResponse.builder()
+                            .code(500)
+                            .message(e.getMessage()).build());
+        }
+    }
+
+    @DeleteMapping
+    public ResponseEntity<StandardResponse> delete(@RequestParam String isbn) {
+        try {
+            return ResponseEntity.ok(
+                    StandardResponse.builder()
+                            .code(200)
+                            .message("Success")
+                            .data(bookService.delete(isbn)).build());
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(
+                    StandardResponse.builder()
+                            .code(500)
+                            .message(e.getMessage()).build());
+        }
+    }
 }
