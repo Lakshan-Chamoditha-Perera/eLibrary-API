9d653cb673837a04c4bb33803c304415e2fc7b8c
Lakshan Chamoditha Perera <perera.alc2000@gmail.com>
2024-01-25 11:05:27 +0530

    Author validations added

--- src/main/java/com/elibrary/library/dto/AuthorDto.java
+++ src/main/java/com/elibrary/library/dto/AuthorDto.java
@@ -5,13 +5,27 @@ import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Data;
 
+import java.sql.Date;
+
 @Data
 @Builder
 public class AuthorDto implements SuperDto {
+    @NotNull(message = "Author ID cannot be null")
+    @Pattern(regexp = "A\\d{3}", message = "Author ID should start with A and have 3 digits")
     private String id;
+    @NotNull(message = "First name cannot be null")
+    @Pattern(regexp = "[A-Za-z ]+", message = "First name should only contain alphabetic characters and spaces")
     private String firstName;
+    @NotNull(message = "Last name cannot be null")
+    @Pattern(regexp = "[A-Za-z ]+", message = "Last name should only contain alphabetic characters and spaces")
     private String lastName;
+    @NotNull(message = "Email cannot be null")
+    @Pattern(regexp = "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}", message = "Email should be in the format of <username>@<domain>.<extension>")
     private String email;
+    @NotNull(message = "Biography cannot be null")
+    @Pattern(regexp = "[A-Za-z0-9 .,]+", message = "Biography should only contain alphabetic characters, numbers, spaces, commas and periods")
     private String biography;
-    private Integer birthYear;
+    @NotNull(message = "Birth year cannot be null")
+    @Pattern(regexp = "\\d{4}", message = "Birth year should have 4 digits")
+    private Date dateOfBirth;
 }
