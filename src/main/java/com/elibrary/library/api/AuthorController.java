dc13675209cc67025afaaa83a6f103870bf8bbb2
Lakshan Chamoditha Perera <perera.alc2000@gmail.com>
2024-01-25 11:39:47 +0530

    Author CURD - API layer

--- src/main/java/com/elibrary/library/api/AuthorController.java
+++ src/main/java/com/elibrary/library/api/AuthorController.java
@@ -3,13 +3,13 @@ package com.elibrary.library.api;
 import com.elibrary.library.dto.AuthorDto;
 import com.elibrary.library.payload.StandardResponse;
 import com.elibrary.library.service.custom.AuthorService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/authors")
@@ -51,9 +51,13 @@ public class AuthorController {
     }
 
     @PostMapping("/save")
-    public ResponseEntity<StandardResponse> save( AuthorDto dto) {
+    public ResponseEntity<StandardResponse> save(@Valid @RequestBody AuthorDto dto, BindingResult bindingResult) {
         try {
-            validateAuthor(dto);
+            if (bindingResult.hasErrors()) {
+                return ResponseEntity.status(
+                        HttpStatus.BAD_REQUEST).body(
+                        new StandardResponse(500, "Fail", bindingResult.getAllErrors()));
+            }
             return ResponseEntity.ok(
                     StandardResponse.builder()
                             .code(200)
@@ -68,8 +72,61 @@ public class AuthorController {
         }
     }
 
-    private void validateAuthor(AuthorDto dto) {
+    @GetMapping("/get")
+    public ResponseEntity<StandardResponse> get(@RequestParam String id) {
+        try {
+            return ResponseEntity.ok(
+                    StandardResponse.builder()
+                            .code(200)
+                            .message("Success")
+                            .data(authorService.get(id)).build());
 
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(
+                    StandardResponse.builder()
+                            .code(500)
+                            .message(e.getMessage()).build());
+        }
     }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<StandardResponse> delete(@RequestParam String id) {
+        try {
+            return ResponseEntity.ok(
+                    StandardResponse.builder()
+                            .code(200)
+                            .message("Success")
+                            .data(authorService.delete(id)).build());
+
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(
+                    StandardResponse.builder()
+                            .code(500)
+                            .message(e.getMessage()).build());
+        }
+    }
+
+    @PatchMapping("/update")
+    public ResponseEntity<StandardResponse> update(@Valid @RequestBody AuthorDto dto, BindingResult bindingResult) {
+        try {
+            if (bindingResult.hasErrors()) {
+                return ResponseEntity.status(
+                        HttpStatus.BAD_REQUEST).body(
+                        new StandardResponse(500, "Fail", bindingResult.getAllErrors()));
+            }
+            return ResponseEntity.ok(
+                    StandardResponse.builder()
+                            .code(200)
+                            .message("Success")
+                            .data(authorService.update(dto)).build());
+
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(
+                    StandardResponse.builder()
+                            .code(500)
+                            .message(e.getMessage()).build());
+        }
+    }
+
 
 }
